4dd8d88ff9907779bfff04625839ef56a9f67f8b
Chris Lloyd <christopher.lloyd@gmail.com>
2011-06-26 19:05:56 +1000

    Blürg.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Minefold::Application.routes.draw do
   root :to => 'home#public'
 
   # Signup
-  get  '/signup' => 'user#new', :as => :new_user
-  post '/signup' => 'user#create', :as => :user
+  get  '/signup' => 'user#new', :as => :user
+  post '/signup' => 'user#create', :as => :users
 
   # Players
   # get  '/:username' => 'user#show'
