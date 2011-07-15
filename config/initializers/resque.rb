0c0c5167631cf665582145302193349eb0e0bac7
Chris Lloyd <christopher.lloyd@gmail.com>
2011-07-15 17:20:34 +1000

    Turn off resque.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,12 +1,12 @@
 # require File.join(Rails.root, 'app', 'models', 'jobs', 'base')
-Dir[File.join(Rails.root, 'app', 'models', 'jobs', '*.rb')].each { |file| require file }
-
-require 'resque/server'
-
-unless ENV["REDISTOGO_URL"].blank?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+# Dir[File.join(Rails.root, 'app', 'models', 'jobs', '*.rb')].each { |file| require file }
+#
+# require 'resque/server'
+#
+# unless ENV["REDISTOGO_URL"].blank?
+#   uri = URI.parse(ENV["REDISTOGO_URL"])
+#   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# end
 
 # Resque::Server.use Rack::Auth::Basic do |username, password|
 #   # NOTE: This is just an authentication for localhost:3000/resque
