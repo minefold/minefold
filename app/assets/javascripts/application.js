24180ce755487c0895867451eb0decf24ea3f1cf
Chris Lloyd <christopher.lloyd@gmail.com>
2011-07-18 00:19:41 +1000

    Get js working on order page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-var products = $('#products li');
-products.click(function(e) {
-  var self = $(e.currentTarget);
-  products.removeClass('active');
-  self.addClass('active').find('input:radio').click();
-  e.preventDefault();
-});
