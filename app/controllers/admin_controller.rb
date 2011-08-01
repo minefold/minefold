0519ce191c3a49026bf5f371b3da973c8dd08d05
Dave Newman <dave@snappyco.de>
2011-08-01 20:18:14 +1000

    showing connected players in admin section

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,6 +7,19 @@ class AdminController < ApplicationController
     @invites_claimed = Invite.claimed.count
     @invites_unclaimed = Invite.unclaimed.count
     @invites = Invite.unclaimed.order(:created_at.asc).all
+    
+    @world_players = []
+    worlds = World.find REDIS.smembers("world:running")
+    worlds.map do |world|
+      player_ids = REDIS.smembers "world:#{world.id}:connected_players"
+      if player_ids.size > 0
+        @world_players << {
+          world: world,
+          players: User.find(player_ids)
+        }
+      end
+    end
+
     render :layout => false
   end
 
