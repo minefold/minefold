380fee03d8cbbbc95b3418cfd1db7b98fa6ead66
Chris Lloyd <christopher.lloyd@gmail.com>
2011-08-10 16:28:48 +1000

    Move to .env which is used by Foreman.

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Simulate Heroku-style ENV var loading
-if vars = File.expand_path('../env.yml', __FILE__) and File.exist?(vars)
-  YAML.load_file(vars)[Rails.env].
-    each {|key, var| ENV[key.to_s.upcase] ||= var.to_s}
-end
-
 # Initialize the rails application
 Minefold::Application.initialize!
