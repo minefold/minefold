5bf1c5fe69ddfab51ecd124b2c5be480c6de39b0
Chris Lloyd <christopher.lloyd@gmail.com>
2011-08-10 17:50:14 +1000

    Read .env file now to play nice with Foreman.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+env_file = Rails.root.join('.env')
+if File.exist?(env_file)
+  File.readlines(env_file).each do |line|
+    next if line =~ /^\s*$/
+    key, val = line.split('=', 2)
+    ENV[key.strip] ||= val.strip
+  end
+end
+
 # Initialize the rails application
 Minefold::Application.initialize!
