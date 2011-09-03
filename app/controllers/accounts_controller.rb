e6990f21c3e225db57b97c0ef0c78df5a97c8e61
Chris Lloyd <christopher.lloyd@gmail.com>
2011-09-03 20:59:21 +1000

    Converted styles to Museo rounded.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,6 +2,9 @@ class AccountsController < ApplicationController
 
   prepend_before_filter :authenticate_user!
 
+  def show
+  end
+
   def dashboard
     @worlds = World.visible.order_by(:slug).all
   end
