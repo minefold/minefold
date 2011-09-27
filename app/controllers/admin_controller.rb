d9bda9618ce6c3fa788985586c25a72efffab4cc
Chris Lloyd <christopher.lloyd@gmail.com>
2011-09-27 14:44:41 +1000

    Add number of current players to the admin.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,7 @@ class AdminController < ApplicationController
   layout nil
 
   def index
+    @current_players_count = REDIS.scard 'players:playing'
     @users = User.order_by([:created_at, :desc]).all
   end
 
