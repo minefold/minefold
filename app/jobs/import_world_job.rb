57a4fcd92f0719c461b18a2602c2559debe79115
Chris Lloyd <christopher.lloyd@gmail.com>
2011-09-29 13:57:02 +1000

    Refactoring import world job.

--- app/jobs/import_world_job.rb
+++ app/jobs/import_world_job.rb
@@ -7,141 +7,142 @@ require 'fileutils'
 class ImportWorldJob
   @queue = :low
 
-  class << self
+  def self.perform(world_upload_id)
+    new.process WorldUpload.find(world_upload_id)
+  end
 
-    def perform world_upload_id
-      pusher_key = "#{name}-#{world_upload_id}"
-      pusher = Pusher[pusher_key]
+  def process(upload)
+    pusher_key = "#{name}-#{world_upload_id}"
+    pusher = Pusher[pusher_key]
 
-      error = nil
-      begin
-        world_upload = process_world_upload WorldUpload.find(world_upload_id)
-        error = world_upload.process_result
-      rescue => e
-        error = e.to_s
-      end
+    error = nil
+    begin
+      world_upload = process_world_upload upload
+      error = world_upload.process_result
+    rescue => e
+      error = e.to_s
+    end
 
-      if error
-        puts "Job failed. #{error}"
-        pusher.trigger 'error', error
+    if error
+      puts "Job failed. #{error}"
+      pusher.trigger 'error', error
+    else
+      puts "Work complete."
+
+      world = world_upload.world
+      pusher.trigger 'success',
+        world_upload: {
+          filename: world_upload.filename,
+            s3_key: world_upload.s3_key,
+        },
+        world: {
+          '_id' => "#{world.id}",
+          name: world.name,
+          slug: world.slug,
+          url:  Minefold::Application.routes.url_for(
+            controller: 'worlds',
+            action:     'show',
+            user_id:    world.owner.slug,
+            id:         world.slug,
+            only_path:  true
+          )
+        }
+    end
+  end
+
+  def process_world_upload world_upload
+    FileUtils.mkdir_p tmp_dir
+    world_upload_archive = download_world_upload world_upload.s3_key
+
+    result = validate_world_archive world_upload_archive
+    if result[:error]
+      world_upload.process_result = result[:error]
+      world_upload.save
+    else
+      world_name = File.basename world_upload.filename, ".*"
+      puts "Creating world:#{world_name} creator:#{world_upload.uploader.username}"
+
+      world = World.create name:world_name,
+                        creator:world_upload.uploader,
+                          owner:world_upload.uploader
+
+      world_archive = create_world_archive world.id, world_upload_archive, result[:world_path]
+
+      upload_world_archive world_archive, world.id
+      world_upload.world = world
+      world_upload.save
+
+      world_upload.uploader.current_world = world
+      world_upload.uploader.save
+    end
+    world_upload
+  end
+
+  def download_world_upload s3_key
+    local_filename = "#{tmp_dir}/#{s3_key}"
+    puts "Downloading #{s3_key} => #{local_filename}"
+
+    worlds_to_import = storage.directories.create :key => "minefold.#{Rails.env}.worlds.uploads"
+    remote_file = worlds_to_import.files.get s3_key
+    File.open(local_filename, 'wb') {|local_file| local_file.write remote_file.body }
+    puts "Download finished"
+
+    local_filename
+  end
+
+  def upload_world_archive world_archive, world_id
+    puts "Uploading #{world_archive}"
+    directory = storage.directories.create :key => "minefold.#{Rails.env}.worlds"
+    directory.files.create key:"#{world_id}.tar.gz",
+                          body:File.open(world_archive),
+                        public:false
+    puts "Upload finished"
+  end
+
+  def validate_world_archive world_upload_archive
+    begin
+      leveldat_file = Zip::ZipFile.foreach(world_upload_archive).find {|zf| zf.name =~ /level\.dat$/ }
+      if leveldat_file
+        { world_path: File.dirname(leveldat_file.name) }
       else
-        puts "Work complete."
-
-        world = world_upload.world
-        pusher.trigger 'success',
-          world_upload: {
-            filename: world_upload.filename,
-              s3_key: world_upload.s3_key,
-          },
-          world: {
-            '_id' => "#{world.id}",
-            name: world.name,
-            slug: world.slug,
-            url:  Minefold::Application.routes.url_for(
-              controller: 'worlds',
-              action:     'show',
-              user_id:    world.owner.slug,
-              id:         world.slug,
-              only_path:  true
-            )
-          }
+        { error: 'level.dat file not present in archive' }
       end
+    rescue => e
+      { error: e.to_s }
     end
+  end
 
-    def process_world_upload world_upload
-      FileUtils.mkdir_p tmp_dir
-      world_upload_archive = download_world_upload world_upload.s3_key
+  def create_world_archive world_id, world_upload_archive, archive_world_path
+    extract_path = "#{tmp_dir}/#{world_id}/extract"
 
-      result = validate_world_archive world_upload_archive
-      if result[:error]
-        world_upload.process_result = result[:error]
-        world_upload.save
-      else
-        world_name = File.basename world_upload.filename, ".*"
-        puts "Creating world:#{world_name} creator:#{world_upload.uploader.username}"
+    extract_archive world_upload_archive, extract_path
 
-        world = World.create name:world_name,
-                          creator:world_upload.uploader,
-                            owner:world_upload.uploader
+    import_path = "#{tmp_dir}/#{world_id}/import/#{world_id}/#{world_id}"
+    FileUtils.mkdir_p import_path
+    FileUtils.cp_r "#{extract_path}/#{archive_world_path}/.", import_path
 
-        world_archive = create_world_archive world.id, world_upload_archive, result[:world_path]
-
-        upload_world_archive world_archive, world.id
-        world_upload.world = world
-        world_upload.save
-
-        world_upload.uploader.current_world = world
-        world_upload.uploader.save
-      end
-      world_upload
+    Dir.chdir "#{tmp_dir}/#{world_id}/import" do
+      TarGz.new.archive world_id, "#{world_id}.tar.gz", exclude:'server.jar'
     end
+    "#{tmp_dir}/#{world_id}/import/#{world_id}.tar.gz"
+  end
 
-    def download_world_upload s3_key
-      local_filename = "#{tmp_dir}/#{s3_key}"
-      puts "Downloading #{s3_key} => #{local_filename}"
+private
 
-      worlds_to_import = storage.directories.create :key => "minefold.#{Rails.env}.worlds-to-import"
-      remote_file = worlds_to_import.files.get s3_key
-      File.open(local_filename, 'wb') {|local_file| local_file.write remote_file.body }
-      puts "Download finished"
+  def storage
+    @storage ||= Fog::Storage.new provider: 'AWS',
+                         aws_access_key_id: ENV['S3_KEY'],
+                     aws_secret_access_key: ENV['S3_SECRET']
+  end
 
-      local_filename
+  def extract_archive archive, destination
+    Zip::ZipFile.foreach(archive).select{|zf| zf.file? }.each do |zf|
+      FileUtils.mkdir_p File.dirname("#{destination}/#{zf.name}")
+      zf.extract "#{destination}/#{zf.name}"
     end
+  end
 
-    def upload_world_archive world_archive, world_id
-      puts "Uploading #{world_archive}"
-      directory = storage.directories.create :key => "minefold.#{Rails.env}.worlds"
-      directory.files.create key:"#{world_id}.tar.gz",
-                            body:File.open(world_archive),
-                          public:false
-      puts "Upload finished"
-    end
-
-    def validate_world_archive world_upload_archive
-      begin
-        leveldat_file = Zip::ZipFile.foreach(world_upload_archive).find {|zf| zf.name =~ /level\.dat$/ }
-        if leveldat_file
-          { world_path: File.dirname(leveldat_file.name) }
-        else
-          { error: 'level.dat file not present in archive' }
-        end
-      rescue => e
-        { error: e.to_s }
-      end
-    end
-
-    def create_world_archive world_id, world_upload_archive, archive_world_path
-      extract_path = "#{tmp_dir}/#{world_id}/extract"
-
-      extract_archive world_upload_archive, extract_path
-
-      import_path = "#{tmp_dir}/#{world_id}/import/#{world_id}/#{world_id}"
-      FileUtils.mkdir_p import_path
-      FileUtils.cp_r "#{extract_path}/#{archive_world_path}/.", import_path
-
-      Dir.chdir "#{tmp_dir}/#{world_id}/import" do
-        TarGz.new.archive world_id, "#{world_id}.tar.gz", exclude:'server.jar'
-      end
-      "#{tmp_dir}/#{world_id}/import/#{world_id}.tar.gz"
-    end
-
-  private
-
-    def storage
-      @storage ||= Fog::Storage.new provider: 'AWS',
-                           aws_access_key_id: ENV['S3_KEY'],
-                       aws_secret_access_key: ENV['S3_SECRET']
-    end
-
-    def extract_archive archive, destination
-      Zip::ZipFile.foreach(archive).select{|zf| zf.file? }.each do |zf|
-        FileUtils.mkdir_p File.dirname("#{destination}/#{zf.name}")
-        zf.extract "#{destination}/#{zf.name}"
-      end
-    end
-
-    def tmp_dir
-      File.join Rails.root, "tmp", "world_uploads"
-    end
+  def tmp_dir
+    File.join Rails.root, "tmp", "world_uploads"
   end
 end
