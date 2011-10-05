c8da1c4a77585222c7097a1e0a1996c12d996969
Chris Lloyd <christopher.lloyd@gmail.com>
2011-10-06 01:54:23 +1100

    Whoops.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_public = true
-  config.fog_host   = 'https://avatars-production-minefold.minefold.com'
+  config.fog_host   = 'https://avatars-production-minefold.s3.amazonaws.com'
   config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
 end
