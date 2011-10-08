c88f09de189074b07f0e52c4e2cf613657e813cd
Dave Newman <dave@snappyco.de>
2011-10-08 16:14:07 +1100

    no tweet on player connected

--- app/jobs/player_connected_job.rb
+++ app/jobs/player_connected_job.rb
@@ -2,9 +2,9 @@ class PlayerConnectedJob < TweetJob
   @queue = :high
 
   def self.perform username, connected_at
-    %W(whatupdave chrislloyd).each do |dude|
-      tweet dude, "#{username} just connected"
-    end
+    # %W(whatupdave chrislloyd).each do |dude|
+    #   tweet dude, "#{username} just connected"
+    # end
   end
 
 end
