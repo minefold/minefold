fb7f290e211a993d991584eada2cf2a025d3b96a
Dave Newman <dave@snappyco.de>
2011-11-06 22:24:31 +1100

    FIX more world_url fixes and specs for world create

--- spec/controllers/worlds_controller_spec.rb
+++ spec/controllers/worlds_controller_spec.rb
@@ -45,21 +45,15 @@ describe WorldsController do
     end
   end
   
-  # describe "create" do
-  #   let(:world) {build(:world)}
-  # 
-  #   it "redirects to the world's page" do
-  #     post :create, world: {name: world.name}
-  # 
-  #     assigns[:world].should_not be_nil
-  # 
-  #     response.should be_redirect
-  #     response.should redirect_to(user_world_path(@world.creator, @world))
-  #   end
-  # 
-  #   it "sets the user's current world" do
-  #     user.reload
-  #     user.current_world.should == assigns[:world]
-  #   end
-  # end
+  describe "create" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+    
+    it "redirects to the world's page" do
+      post :create, world: { name: 'Sluggy' }
+      response.should redirect_to(world_path('sluggy'))
+    end
+  end
 end
