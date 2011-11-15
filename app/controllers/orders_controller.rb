841e7729a1e8a05f453694935a0efd6e7434c928
Dave Newman <dave@snappyco.de>
2011-11-14 22:27:56 -0800

    flash message on plan change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,8 @@ class OrdersController < ApplicationController
   
   def create
     current_user.update_attributes! params[:user]
-    redirect_to params[:return_url]
+    new_plan = Plan.find(params[:user][:plan_id])
+    redirect_to params[:return_url], notice: "Plan changed to #{new_plan.name}"
   end
 
 end
