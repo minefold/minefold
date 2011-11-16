6f6d6f1b9b2b83686563f6f8d83994aec1117b8c
Dave Newman <dave@snappyco.de>
2011-11-16 11:59:22 -0800

    Hiding plans

--- app/models/time_pack.rb
+++ app/models/time_pack.rb
@@ -1,6 +1,6 @@
 class TimePack < Struct.new(:hours, :price)
   def self.all
-    [new(10, 295), new(40, 795), new(120, 1195)]
+    [new(10, 295), new(40, 795), new(200, 1995)]
   end
   
   def self.find hours
