9df00542a31f4caff0bad82067d6c1cf192d36f8
Chris Lloyd <christopher.lloyd@gmail.com>
2011-11-16 12:22:49 -0800

    Prettier plans.

--- app/models/time_pack.rb
+++ app/models/time_pack.rb
@@ -2,8 +2,13 @@ class TimePack < Struct.new(:hours, :price)
   def self.all
     [new(10, 295), new(40, 795), new(200, 1995)]
   end
-  
+
   def self.find hours
     all.find{|p| p.hours == hours}
   end
+
+  def rate
+    (((price / 100.0) / hours) * 100).ceil
+  end
+
 end
