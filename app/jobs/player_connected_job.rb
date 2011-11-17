37b2572067952485ff47d2722c5f2e91507217ed
Dave Newman <dave@snappyco.de>
2011-11-16 22:36:28 -0800

    player connected job doesnt do anything right now

--- app/jobs/player_connected_job.rb
+++ app/jobs/player_connected_job.rb
@@ -2,9 +2,9 @@ class PlayerConnectedJob < TweetJob
   @queue = :high
 
   def self.perform username, connected_at
-    if user = User.first(conditions: { username:username })
-      user.played!
-    end
+    # if user = User.first(conditions: { username:username })
+    #   user.played!
+    # end
     
     # %W(whatupdave chrislloyd).each do |dude|
     #   tweet dude, "#{username} just connected"
