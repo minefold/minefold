984e1da12f59bc9c6aa1390588bccab51a88f6c2
Dave Newman <dave@snappyco.de>
2011-12-11 17:41:19 +1100

    better from address

--- app/mailers/world_mailer.rb
+++ app/mailers/world_mailer.rb
@@ -1,7 +1,7 @@
 class WorldMailer < ActionMailer::Base
   include Resque::Mailer
 
-  default from: 'team@minefold.com'
+  default from: 'Minefold <team@minefold.com>'
 
   def play_request(world_id, player_id)
     @world    = World.find(world_id)
@@ -20,10 +20,6 @@ class WorldMailer < ActionMailer::Base
 
     mail   to: @player.email,
       subject: "#{@creator.username} has added you #{@world.name}"
-  end
-  
-  def self.deliver_to_all!(world, ignore)
-    world.players
   end
   
   def world_started world_id, player_id
