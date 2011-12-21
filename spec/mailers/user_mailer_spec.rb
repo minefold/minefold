007362f69d20a774cea5d60a648958a4b545878c
Dave Newman <dave@snappyco.de>
2011-12-21 11:58:58 -0800

    Fix reminder email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,22 +1,18 @@
 require "spec_helper"
 
 describe UserMailer do
-  # describe "invite" do
-  #   let(:user)   { build :user }
-  #   let(:world)  { build :world, creator: user}
-  #   let(:mail)   { UserMailer.invite user, world, 'chris@minefold.com' }
-  #
-  #   it "renders the headers" do
-  #     mail.subject.should eq("#{user.username} wants you to play in #{world.name}")
-  #     mail.to.should eq(["chris@minefold.com"])
-  #     mail.from.should eq(["team@minefold.com"])
-  #   end
-  #
-  #   it "renders the body" do
-  #     mail.body.encoded.should include(world.name)
-  #     mail.body.encoded.should include(world.slug)
-  #     mail.body.encoded.should include(user.referral_code)
-  #   end
-  # end
+  describe "reminder" do
+    let(:user) { create :user }
+    let(:mail) { UserMailer.reminder user.id }
+    
+    it "renders the headers" do
+      mail.subject.should eq("Buy more Minefold time (running low)")
+      mail.to.should eq([user.email])
+    end
 
+    it "renders the body" do
+      mail.body.encoded.should include(user.username)
+      mail.body.encoded.should include(time_account_url)
+    end
+  end
 end
