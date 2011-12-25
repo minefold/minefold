891c8bfd69c7fb3db4a6c78346615b6625a28333
Dave Newman <dave@snappyco.de>
2011-12-25 15:54:03 -0800

    Add jobs for op and deop

--- app/jobs/player_deopped_job.rb
+++ app/jobs/player_deopped_job.rb
@@ -1,10 +1,10 @@
-class PlayerOppedJob < TweetJob
-  @queue = :high
+class PlayerDeoppedJob < TweetJob
+  @queue = :low
 
   def self.perform world_id, username
     user = User.by_username(User.sanitize_username(username)).first
     membership = World.find(world_id).memberships.where(user_id: user.id).first
-    membership.role = 'op'
+    membership.role = 'player'
     membership.save
   end
 end
