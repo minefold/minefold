41db68fdf079fd530e269dc18c81fd358813165f
Chris Lloyd <christopher.lloyd@gmail.com>
2011-12-27 00:12:17 +1100

    Fark, typo in production.

--- app/helpers/mixpanel_helper.rb
+++ app/helpers/mixpanel_helper.rb
@@ -2,7 +2,7 @@ module MixpanelHelper
   def track_pageview
     if Rails.env.production?
       code = "mpq.track('page viewed', {'page name':document.title, 'url':window.location.pathname});"
-      head {haml_tag('script', tracking_code)}
+      head {haml_tag('script', code)}
     end
   end
 end
