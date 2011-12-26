e6287c65a1eb71845805e721c16d038730fa69b7
Chris Lloyd <christopher.lloyd@gmail.com>
2011-12-27 00:43:18 +1100

    More tired stuff.

--- app/helpers/mixpanel_helper.rb
+++ app/helpers/mixpanel_helper.rb
@@ -1,15 +1,15 @@
 module MixpanelHelper
   
-  def track(name, properties={})
+  def track(name, properties={g})
     if Rails.env.production?
-      code = "mpq.track(#{name.to_json}, #{properties.to_json});"
+      code = "mpq.track(#{name.to_json}, #{properties.to_json});".html_safe
       head {haml_tag('script', code)}
     end
   end
   
   def track_pageview
     if Rails.env.production?
-      code = "mpq.track('page viewed', {'page name':document.title, 'url':window.location.pathname});"
+      code = "mpq.track('page viewed', {'page name':document.title, 'url':window.location.pathname});".html_safe
       head {haml_tag('script', code)}
     end
   end
