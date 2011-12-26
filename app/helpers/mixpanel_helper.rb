0d07522cfc3d042829055d4888a2c1604421b7bd
Chris Lloyd <christopher.lloyd@gmail.com>
2011-12-27 00:06:39 +1100

    Switches to better pageview tracking.
    As recommended by https://mixpanel.com/docs/getting-started/events-vs-page-views

--- app/helpers/mixpanel_helper.rb
+++ app/helpers/mixpanel_helper.rb
@@ -1,10 +1,8 @@
 module MixpanelHelper
-
-  def track(event, properties={})
+  def track_pageview
     if Rails.env.production?
-      tracking_code = "mpq.track(#{event.to_json},#{properties.to_json})".html_safe
-      head { haml_tag('script', tracking_code)}
+      code = "mpq.track('page viewed', {'page name':document.title, 'url':window.location.pathname});"
+      head {haml_tag('script', tracking_code)}
     end
   end
-
 end
