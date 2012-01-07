77534698c1c201df81ef3e4f3cad1b05e1cef5ab
Chris Lloyd <christopher.lloyd@gmail.com>
2012-01-07 13:58:25 -0800

    Fixed PlayerOppedJob spec.

--- app/jobs/player_opped_job.rb
+++ app/jobs/player_opped_job.rb
@@ -1,10 +1,16 @@
-class PlayerOppedJob < TweetJob
+class PlayerOppedJob
   @queue = :low
 
-  def self.perform world_id, username
-    user = User.by_username(User.sanitize_username(username)).first
-    membership = World.find(world_id).memberships.where(user_id: user.id).first
-    membership.role = 'op'
-    membership.save
+  def self.perform(world_id, player_username)
+    world = World.find(world_id)
+    user = User.by_username(player_username).first
+
+    new.process! world, user
+  end
+
+  def process!(world, user)
+    membership = world.memberships.find_or_initialize_by(user_id: user.id)
+    membership.role = Membership::OP
+    world.save
   end
 end
