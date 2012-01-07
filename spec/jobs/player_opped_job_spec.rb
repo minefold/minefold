c3b0faac8466ebfa321127ac828f9d92268e68a2
Chris Lloyd <christopher.lloyd@gmail.com>
2012-01-07 13:58:25 -0800

    Fixed PlayerOppedJob spec.

--- spec/jobs/player_opped_job_spec.rb
+++ spec/jobs/player_opped_job_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe PlayerOppedJob do
   let(:creator) { create :user }
   let(:world)   { create :world, creator: creator }
-  
+
   let(:player)  { create :user }
-  
-  before { world.add_player player }
-  
-  it "should op player" do
-    PlayerOppedJob.perform world.id, player.username
-    world.ops.to_a.should eq([creator, player])
+
+  before { world.add_member(player) }
+
+  it "makes the player an op" do
+    PlayerOppedJob.new.process! world, player
+    world.ops.should include(player)
   end
 end
