0110d00dc757c7f5e44d73b074fc21b4bfbb46a6
Dave Newman <dave@snappyco.de>
2012-01-09 14:37:23 -0800

    Fix bug in player opped job

--- app/jobs/player_opped_job.rb
+++ app/jobs/player_opped_job.rb
@@ -2,9 +2,11 @@ class PlayerOppedJob < TweetJob
   @queue = :low
 
   def self.perform world_id, username
-    user = User.by_username(User.sanitize_username(username)).first
-    membership = World.find(world_id).memberships.where(user_id: user.id).first
-    membership.role = 'op'
-    membership.save
+    user = User.by_username(username).first
+    if user
+      membership = World.find(world_id).memberships.where(user_id: user.id).first
+      membership.role = 'op'
+      membership.save
+    end
   end
 end
