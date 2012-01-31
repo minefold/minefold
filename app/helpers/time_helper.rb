3b0e04496a72badfbdf390aa45b8fb854761d85a
Chris Lloyd <christopher.lloyd@gmail.com>
2012-01-30 18:27:51 -0800

    Cleans up styles on world members page.

--- app/helpers/time_helper.rb
+++
@@ -1,8 +0,0 @@
-module TimeHelper
-
-  def time_ago_in_words(time, options={}, &blk)
-    options[:class] ||= 'timeago'
-    content_tag(:time, time.to_s, options.merge(datetime: time.getutc.iso8601))
-  end
-
-end
