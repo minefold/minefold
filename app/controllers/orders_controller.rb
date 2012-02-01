69bed8dbfa722c8a385c6b7afc90dfee32fd5194
Dave Newman <dave@snappyco.de>
2012-01-31 16:04:12 -0800

    Change time packs to month packs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,15 @@ class OrdersController < ApplicationController
   prepend_before_filter :authenticate_user!
   
   def create
-    # TODO: When we start modifying the TimePacks we need to have actual IDs for them. If we change the price of a pack while somebody is on the checkout page they could get a nasty surprise with their bill.
-    return payment_required! unless params[:stripe_token] and params[:hours]
+    return payment_required! unless params[:stripe_token] and params[:pack_id]
     
-    pack = TimePack.find(params[:hours].to_i)
+    pack = TimePack.find params[:pack_id]
     
     return payment_required! unless pack
     
-    if current_user.buy_time!(params[:stripe_token], pack)
-      track 'paid', hours: pack.hours, amount: pack.amount
-      redirect_to user_root_path, notice: "Thank you for buying #{pack.hours} hours"
+    if current_user.buy_pack!(params[:stripe_token], pack)
+      track 'paid', months: pack.months, amount: pack.cents
+      redirect_to user_root_path, notice: "Thank you for buying #{pack.months} of Minefold Pro"
     else
       payment_required!
     end
