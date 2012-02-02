8ecf097c9090b542c44b3caa26286a9bb302534d
Myles Byrne <myles@myles.id.au>
2012-02-01 19:50:18 -0600

    hyperdrive

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,12 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET'],
+    # hardcoded for "development speed"
+    aws_access_key_id:  ENV['S3_KEY'] || 'AKIAJPN5IJVEBB2QE35A',
+    aws_secret_access_key: ENV['S3_SECRET'] || '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',
     region:'us-east-1'
   }
-  
+
   config.fog_public = true
   config.fog_attributes = {
     'Cache-Control' => "max-age=#{10.years.to_i}"
