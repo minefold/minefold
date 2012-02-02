477f670863e1c15c4560d62654cafb9c6505981b
Myles Byrne <myles@myles.id.au>
2012-02-01 19:59:42 -0600

    api_auth guard method

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,12 +1,21 @@
 class Api::ApiController < ActionController::Base
   include Mixpanel
 
-  before_filter -> {
+  before_filter :http_auth
+  layout nil
+
+  def http_auth
     @current_user = authenticate_with_http_basic do |username, password|
       user = User.by_email_or_username(username).first
       user if user && user.valid_password?(password)
     end
 
-    render status: :unauthorized, layout: false, nothing: true unless @current_user
-  }
+    render status: :unauthorized, text: "HTTP Basic Auth required" unless @current_user
+  end
+
+  def api_key_auth
+    @current_user = User.where(authentication_token: request.headers['Api-key']).first
+    render status: 403, text: "API key required" unless @current_user
+  end
+
 end
---
+++ app/controllers/api/shots_controller.rb
@@ -0,0 +1,25 @@
+class Api::ShotsController < Api::ApiController
+
+  skip_before_filter :http_auth
+  before_filter :api_key_auth
+
+  def create
+    sha = Digest::SHA1.hexdigest params[:shot].tempfile.read
+
+    # gaurd against confused client
+    if Shot.where(sha: sha, creator_id: current_user.id).first
+      return render status: :ok, text: "already uploaded: #{sha}\n"
+    end
+
+    shot = Shot.new
+    shot.creator = current_user
+    shot.original_filename = params[:shot].original_filename
+    shot.file = params[:shot]
+    shot.sha = sha
+    shot.save!
+
+    track 'uploaded shot', source: request.headers['User-Agent']
+    render status: :ok, text: "uploaded: #{sha}\n"
+  end
+
+end
