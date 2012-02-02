03858ce20668777db420bb19dd19370eb10491ec
Dave Newman <dave@snappyco.de>
2012-02-02 11:50:23 -0800

    Pivot subscribes paid users to paid users list

--- config/initializers/campaign_monitor.rb
+++ config/initializers/campaign_monitor.rb
@@ -1,1 +1,20 @@
 CreateSend.api_key ENV['CAMPAIGN_MONITOR_API_KEY']
+
+class CampaignList
+  def self.paid_users
+    new('baa78a03c4f61487315f09ac7239dc4a')
+  end
+  
+  def initialize list_id
+    @list_id = list_id
+  end
+  
+  def subscribe user
+    unless CreateSend.api_key.empty?
+      CreateSend::Subscriber.add @list_id, 
+        user.email, user.username, [{ :Key => 'Username', :Value => user.username}], true
+    else
+      puts "Fake subscribe #{user} to #{@list_id}"
+    end
+  end
+end
--- db/pivots/20120201202352.rb
+++ db/pivots/20120201202352.rb
@@ -2,9 +2,9 @@
 
 paid_users = (
   Stripe::Customer.all(count: 100).data.
-      map{|c| p c['description'];  c['description'] }.map{|d| id, username = d.split('-'); { id: id, username: username } } +
+      map{|c| c['description'] }.map{|d| id, username = d.split('-'); { id: id, username: username } } +
   Stripe::Charge.all(:count => 100).data.
-      map{|c| p c['description']; c['description'] }.compact.select{|c| c.include? '-' }.map{|d| d.split('-').last }.map{|d| {id: d.split(':').first} }
+      map{|c| c['description'] }.compact.select{|c| c.include? '-' }.map{|d| d.split('-').last }.map{|d| {id: d.split(':').first} }
 )
 
 paid_users.map do |u|
@@ -16,4 +16,6 @@ end.compact.uniq_by{|u| u.id }.sort_by{|u| u.username}.each do |user|
   user.plan_expires_at = Time.now + 12.months
   user.save!
   puts "#{user.username} plan expires: #{user.plan_expires_at}"
+  
+  CampaignList.paid_users.subscribe user
 end
