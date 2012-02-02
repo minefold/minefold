b849045fc53bb603ac9cbdb93e0c324fafe1108d
Dave Newman <dave@snappyco.de>
2012-02-02 15:49:01 -0800

    Fix minute played job

--- app/jobs/minute_played_job.rb
+++ app/jobs/minute_played_job.rb
@@ -2,9 +2,10 @@ class MinutePlayedJob < TweetJob
   @queue = :high
 
   def self.perform(user_id, world_id, timestamp)
+    user = User.find(user_id)
     world = World.find(world_id)
 
-    new.process!(world)
+    new.process!(user, world, timestamp)
   end
 
   def process!(user, world, timestamp)
