cc02ea67b54c8480e02b144dd215faede5c5f7d9
Myles Byrne <myles@myles.id.au>
2012-02-03 15:00:28 -0600

    shuffle

--- app/controllers/shot_albums_controller.rb
+++ app/controllers/shot_albums_controller.rb
@@ -9,15 +9,6 @@ class ShotAlbumsController < ApplicationController
     redirect_to '/shots/admin'
   end
 
-  def admin
-    @shot_album = ShotAlbum.find(params[:id])
-    @shot_albums = ShotAlbum.all(
-      conditions: { creator_id: current_user.id},
-      sort: [[:created_at, :desc]]
-    )
-    render 'shots/album'
-  end
-
   def destroy
     @shot_album = ShotAlbum.find(params[:id])
     if params[:cascade] == "true"
@@ -25,6 +16,17 @@ class ShotAlbumsController < ApplicationController
     end
     @shot_album.delete
     redirect_to '/shots/admin'
+  end
+
+  # ---
+
+  def admin
+    @shot_album = ShotAlbum.find(params[:id])
+    @shot_albums = ShotAlbum.all(
+      conditions: { creator_id: current_user.id},
+      sort: [[:created_at, :desc]]
+    )
+    render 'shots/album'
   end
 
 end
