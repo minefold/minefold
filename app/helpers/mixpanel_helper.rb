d6697ab95d43f4d19ce3b1f9f9f9e45fa6d13251
Chris Lloyd <christopher.lloyd@gmail.com>
2012-02-28 17:12:40 +1100

    Better mixpanel tracking and moving to better asset hosts.

--- app/helpers/mixpanel_helper.rb
+++ app/helpers/mixpanel_helper.rb
@@ -1,16 +1,12 @@
 module MixpanelHelper
 
   def track(name, properties={})
-    if Rails.env.production?
-      code = "mpq.track(#{name.to_json}, #{properties.to_json});".html_safe
-      head { haml_tag('script', code) }
-    end
+    code = "mpq.track(#{name.to_json}, #{properties.to_json});".html_safe
+    haml_tag('script', code)
   end
 
   def track_pageview
-    if Rails.env.production?
-      code = "mpq.track('page viewed', {'page name':document.title, 'url':window.location.pathname});".html_safe
-      head { haml_tag('script', code) }
-    end
+    code = "mpq.track('page viewed', {'page name':document.title, 'url':window.location.pathname});".html_safe
+    haml_tag('script', code)
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Minefold::Application.configure do
   #       verbose: true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://d1gl6072w16l3y.cloudfront.net"
+  config.action_controller.asset_host = ENV['ASSET_HOST']
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
