c5b46ae009ebceee41a3b0f3a5a194a89486d51c
Dave Newman <dave@snappyco.de>
2012-02-29 14:32:18 +1100

    Remember me on users logged in through facebook

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.find_or_create_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
+    @user.remember_me = true
+
     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
     sign_in_and_redirect @user, :event => :authentication
   end
