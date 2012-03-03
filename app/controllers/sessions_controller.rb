5612705e418c4e3031eada343e58c10ef3068fa0
Chris Lloyd <christopher.lloyd@gmail.com>
2012-03-03 16:55:18 +1100

    Stop setting the mpid when somebody signs in, as the current user probably isn't set.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < Devise::SessionsController
 
   def create
-    cookies[:mpid] = current_user.mpid
     super
     track 'signed in', distinct_id: current_user.id.to_s,
                        mp_name_tag: current_user.safe_username
