a7e388c1e8a5a22631ab32bdd59f33914db3c2d5
Dave Newman <dave@snappyco.de>
2012-03-06 23:01:30 -0800

    track player session from disconnection event

--- app/jobs/player_disconnected_job.rb
+++ app/jobs/player_disconnected_job.rb
@@ -1,18 +1,37 @@
 class PlayerDisconnectedJob
   @queue = :high
 
-  # TODO Pass in user_id rather than username
-  # TODO Pass in world too
-  def self.perform(username, disconnected_at)
-    user = User.by_username(username).first
-    world = user.current_world
-    new.process!(user, world, disconnected_at) if world
+  # TODO deploy new prism then change these names
+  def self.perform(user_id_or_username, world_id_or_disconnected_at, connected_at = nil, disconnected_at = nil)
+    user_id = BSON::ObjectId(user_id_or_username.to_s) rescue nil
+    user = user_id ? User.find(user_id) : User.by_username(user_id_or_username).first
+
+    world_id = BSON::ObjectId(world_id_or_disconnected_at.to_s) rescue nil
+    world = world_id ? World.find(world_id) : user.current_world
+
+    if world
+      if disconnected_at
+        new.process!(user, world, connected_at, disconnected_at)
+      else
+        new.process!(user, world, nil, world_id_or_disconnected_at)
+      end
+    end
   end
 
-  def process!(user, world, disconnected_at)
+  def process!(user, world, connected_at, disconnected_at)
     Events::Disconnection.create source: user,
                                  target: world,
                                  created_at: disconnected_at
+
+    if connected_at
+      seconds = disconnected_at - connected_at
+      Mixpanel.track 'played',
+        seconds: seconds,
+        minutes: (seconds / 60.0).to_i,
+        hours: (seconds / 60.0 / 60.0).to_i,
+        distinct_id: user.mpid.to_s,
+        mp_name_tag: user.email
+    end
 
     # TODO Broadcast pusher event
   end
--- spec/jobs/player_disconnected_job_spec.rb
+++ spec/jobs/player_disconnected_job_spec.rb
@@ -9,4 +9,10 @@ describe PlayerDisconnectedJob do
 
     world.events.should have(1).disconnection_event
   end
+
+  it "records new event" do
+    PlayerDisconnectedJob.perform player.id, world.id, Time.now - 120, Time.now
+
+    world.events.should have(1).disconnection_event
+  end
 end
