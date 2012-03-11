7816f4f55aaf7ef1ce0b9a9801e76aa3ae0fbf41
Dave Newman <dave@snappyco.de>
2012-03-10 17:52:57 -0800

    Mixpanel track time played on payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,27 @@
 class OrdersController < ApplicationController
   prepend_before_filter :authenticate_user!
-  
+
   def create
     return payment_required! unless params[:stripe_token] and params[:pack_id]
-    
+
     pack = TimePack.find params[:pack_id]
-    
+
     return payment_required! unless pack
-    
+
     if current_user.buy_pack!(params[:stripe_token], pack)
-      track 'paid', months: pack.months, amount: pack.cents
-      redirect_to user_root_path, notice: "Thank you for buying #{pack.months} of Minefold Pro"
+      minutes_played = current_user.minutes_played || 0
+      track 'paid',
+        months: pack.months,
+        amount: pack.cents,
+        minutes_played: minutes_played,
+        hours_played:(minutes_played / 60)
+
+      redirect_to user_root_path, notice: "Thank you for buying #{pack.months} months of Minefold Pro"
     else
       payment_required!
     end
   end
-  
+
 private
 
   def payment_required!
