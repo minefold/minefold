a28caaa1c294d97c5fbcf47ec9a6b555d2b6d2ca
Dave Newman <dave@snappyco.de>
2012-03-15 21:27:05 -0700

    ignore already unsubscribed players

--- app/jobs/unsubscribe_player_job.rb
+++ app/jobs/unsubscribe_player_job.rb
@@ -4,10 +4,14 @@ class UnsubscribePlayerJob
   def self.perform user_id
     new.process! User.find(user_id)
   end
-  
+
   def process! user
-    subscriber = CreateSend::Subscriber.new ENV['CAMPAIGN_MONITOR_USER_LIST_ID'], user.email
-    subscriber.unsubscribe
-    puts "unsubscribed #{user.id} #{user.username} #{user.email}"
+    begin
+      subscriber = CreateSend::Subscriber.new ENV['CAMPAIGN_MONITOR_USER_LIST_ID'], user.email
+      subscriber.unsubscribe
+      puts "unsubscribed #{user.id} #{user.username} #{user.email}"
+    rescue
+      puts "player already unsubscribed"
+    end
   end
 end
