a0f7d80c721178a896804b1dc920f70942cef7b9
Dave Newman <dave@snappyco.de>
2012-03-29 12:00:10 -0700

    fixed player disconnected job spec

--- spec/jobs/player_disconnected_job_spec.rb
+++ spec/jobs/player_disconnected_job_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PlayerDisconnectedJob do
   let(:world) { Fabricate :world }
-  let(:player) { Fabricate :user, current_world: world }
+  let(:player) { Fabricate :minecraft_player }
 
   it "records event" do
     PlayerDisconnectedJob.perform player.id, world.id, (Time.now - 120).to_s, Time.now.to_s
