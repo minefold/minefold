9cd409fa60096525e4b0ae48eb8739d6553d662b
Dave Newman <dave@snappyco.de>
2012-03-29 12:05:36 -0700

    fixed minute played job

--- spec/jobs/minute_played_job_spec.rb
+++ spec/jobs/minute_played_job_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe MinutePlayedJob do
-  let(:player) { Fabricate(:user) }
+  let(:player) { Fabricate(:minecraft_player) }
   let(:world)  { Fabricate(:world) }
 
-  before { world.add_member(player) }
-
   it "should increase world minutes played" do
-    lambda {
-      subject.process! player, world, Time.now
-    }.should change(world, :minutes_played).by(1)
+    MinutePlayedJob.perform player.id, world.id, Time.now
+    world.reload.minutes_played.should == 1
+  end
+
+  it "should increase player minutes played" do
+    MinutePlayedJob.perform player.id, world.id, Time.now
+    player.reload.minutes_played.should == 1
   end
 end
