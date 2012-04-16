4965d483861f517689dda4fcb0077e5fdce8a89d
Dave Newman <dave@snappyco.de>
2012-04-16 16:26:06 -0700

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User
   include Mongoid::Timestamps
   include Mongoid::Paranoia
   include Verifiable
+  include Referrable
 
 
   attr_accessible :email,
@@ -254,37 +255,6 @@ class User
   def notify?(notification)
     confirmed? and notifications[notification.to_s] != "0"
   end
-
-
-# ---
-# Invites
-
-
-  INVITE_TOKEN_LENGTH = 6
-
-  def self.invite_token_exists?(token)
-    where(invite_token: token).exists?
-  end
-
-  def self.free_invite_token
-    begin
-      token = rand(36 ** INVITE_TOKEN_LENGTH).to_s(36)
-    end while invite_token_exists?(token)
-    token
-  end
-
-  field :invite_token, type: String, default: ->{
-    self.class.free_invite_token
-  }
-  validates_uniqueness_of :invite_token
-
-  belongs_to :referrer, class_name: 'User', inverse_of: :referrals
-  has_many :referrals, class_name: 'User', inverse_of: :referrer
-
-  # def referrer=(user)
-  #   write_attribute :referrer_id, user.id unless user.nil? or self == user
-  # end
-
 
 
 # ---
