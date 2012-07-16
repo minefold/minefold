f4cc9d0f65d6590ffb0e6571dbec7e6f2cc43221
Chris Lloyd <christopher.lloyd@gmail.com>
2012-07-16 14:50:29 -0700

    Set ActionMailer asset host so images show up in HTML emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Minefold::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   config.action_controller.asset_host = ENV['ASSET_HOST']
+  config.action_mailer.asset_host = ENV['ASSET_HOST']
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
