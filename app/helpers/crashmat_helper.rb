0fb6f67ede3ea7875e3be5b7781a24fab9bf0fa3
Chris Lloyd <christopher.lloyd@gmail.com>
2012-07-21 17:41:07 -0700

    Crashm.at error tracking.

---
+++ app/helpers/crashmat_helper.rb
@@ -0,0 +1,17 @@
+module CrashmatHelper
+
+  def crashmat_config
+    {
+      key: ENV['CRASHMAT'],
+      user: crashmat_user
+    }
+  end
+
+  def crashmat_user
+    signed_in? and {
+      id:    current_user.id,
+      email: current_user.email
+    }
+  end
+
+end
