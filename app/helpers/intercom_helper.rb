0d5bba36c1641c8147355448adc8633870758a9f
Chris Lloyd <christopher.lloyd@gmail.com>
2012-07-23 15:29:59 -0700

    Refactor Intercom helpers.

--- app/helpers/intercom_helper.rb
+++ app/helpers/intercom_helper.rb
@@ -3,25 +3,38 @@ module IntercomHelper
   def intercom_settings
     settings = {
       app_id: '8oc9zbvo',
-      custom_data: {},
       widget: {
         label: 'Support'
       }
     }
+
     if signed_in?
-      settings.merge!(
-        email: current_user.email,
-        created_at: current_user.created_at.to_i,
-        user_hash: Digest::SHA1.hexdigest('kczzht6c' + current_user.email)
-      )
-      settings[:custom_data].merge! current_user.tracking_data
+      settings[:email] = current_user.email
+      settings[:name] = current_user.name,
+      settings[:created_at] = current_user.created_at.to_i
+      settings[:user_hash] = Digest::SHA1.hexdigest('kczzht6c' + current_user.email)
+
+      settings[:custom_data] = {
+        'beta?' => current_user.beta?,
+        'created worlds' => current_user.created_worlds.count,
+        'credits' => current_user.credits,
+        'facebook?' => current_user.facebook_linked?,
+        'minutes played' => current_user.minutes_played,
+        'max world players' => current_user.world_player_counts.max,
+        'member worlds' => current_user.worlds.count,
+        'pro?' => current_user.pro?,
+        'profile' => player_url(current_user.minecraft_player),
+        'username' => current_user.username,
+        'verify host' => current_user.verification_host,
+        'verified?' => current_user.verified?,
+      }
     end
 
     settings
   end
 
   def intercom_script_tag
-    js = "intercomSettings = #{intercom_settings.to_json};".html_safe
+    js = "var intercomSettings = #{intercom_settings.to_json};".html_safe
     content_tag :script, js, id: 'IntercomSettingsScriptTag'
   end
 
