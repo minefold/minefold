8f330073050cd32a18bf4a2da6717c42f6c8c973
Chris Lloyd <christopher.lloyd@gmail.com>
2012-08-07 14:20:06 -0700

    Fix already verified job not finding user.

--- app/jobs/user_already_verified_job.rb
+++ app/jobs/user_already_verified_job.rb
@@ -4,6 +4,12 @@ class UserAlreadyVerifiedJob < Job
   def initialize(username, verification_token)
     @username = username
     @user = User.where(verification_token: verification_token).first
+
+    # Weird case where the user doesn't have a verification token
+    # TODO This is a quick fix but need to investigate the root cause.
+    if !@user && player = MinecraftPlayer.by_username(@username).first
+      @user = player.user
+    end
   end
 
   def perform!
