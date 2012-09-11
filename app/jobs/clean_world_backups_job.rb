176e294b9fd915eb7d28501b80a3508241e29235
Dave Newman <dave@snappyco.de>
2012-09-11 14:46:14 -0700

    fixed world backup job

--- app/jobs/clean_world_backups_job.rb
+++ app/jobs/clean_world_backups_job.rb
@@ -2,8 +2,9 @@ class CleanWorldBackupsJob < Job
   @queue = :low
 
   def perform!
-    all_files = page_world_files
-    # all_files.each{|f| puts f.key }
+    file_limit = (ENV['FILE_LIMIT'] || 10000).to_i
+    all_files = page_world_files('minefold-production-worlds', '', file_limit)
+    all_files += page_world_files('minefold-production', 'world-backups', file_limit)
 
     b = backups all_files
 
@@ -17,11 +18,9 @@ class CleanWorldBackupsJob < Job
     ## retention
     # all for an hour
     # hourly for 2 days
-    # daily forever
+    # daily for 14 days
 
     grouped = b.each_with_object({}) do |backup, hash|
-      day = backup[:time].strftime('%Y-%m-%d')
-
       hash[backup[:world_id]] ||= []
       hash[backup[:world_id]] << backup
     end
@@ -34,11 +33,13 @@ class CleanWorldBackupsJob < Job
       rest = backups.sort_by {|b| -b[:time].to_i }
       puts "#{world_id}: (#{backups.size})"
 
+      # all for an hour
       period_end = rest.first[:time] - 1.hour
       in_period, rest = rest.partition {|b| b[:time] > period_end }
-      puts "  all:"
+      puts "  all #{period_end}:"
       in_period.each {|b| puts "    #{b[:time]} #{b[:key]}"}
 
+      # hourly for 2 days
       period_end -= 2.days
       in_period, rest = rest.partition {|b| b[:time] > period_end  }
       in_period.group_by {|b| b[:hour] }.each do |hour, backups|
@@ -50,7 +51,9 @@ class CleanWorldBackupsJob < Job
         end
       end
 
-      in_period = rest.select{|b| b[:time] < period_end }
+      # daily for 14 days
+      period_end -= 14.days
+      in_period, rest = rest.partition{|b| b[:time] > period_end }
       in_period.group_by {|b| b[:day] }.each do |day, backups|
         puts " daily (#{day}):"
         backups.each.with_index do |b, i|
@@ -59,16 +62,27 @@ class CleanWorldBackupsJob < Job
           puts "    #{b[:time]} #{b[:key]} #{'delete' if delete}"
         end
       end
+
+      # remove the rest
+      in_period = rest
+      puts " older than (#{period_end}):"
+      in_period.each.with_index do |b, i|
+        delete = true
+        to_delete << b if delete
+        puts "    #{b[:time]} #{b[:key]} #{'delete' if delete}"
+      end
     end
 
     puts "Deleting #{to_delete.size} files (#{sum_gb to_delete} Gb)"
-    
+
     i = 0
     to_delete.each do |backup|
       i += 1
-      puts "  delete (#{i}/#{to_delete.size}): #{backup[:key]}"
-      backup[:file].destroy
+      puts "  delete (#{i}/#{to_delete.size}): #{backup[:key]}" unless ENV['DRY_RUN']
+      # backup[:file].destroy # unless ENV['DRY_RUN']
     end
+
+    puts "Deleting #{to_delete.size} files (#{sum_gb to_delete} Gb)"
   end
 
   def storage
@@ -81,17 +95,22 @@ class CleanWorldBackupsJob < Job
     (backups.inject(0) {|acc, b| acc + b[:content_length] }) / 1024.0 / 1024.0 / 1024.0
   end
 
-  def page_world_files
-    dir = storage.directories.create key: 'minefold-production-worlds' #  ENV['WORLDS_BUCKET']
+  def page_world_files key, prefix = '', limit = 10000
+    dir = storage.directories.create key: key #  ENV['WORLDS_BUCKET']
 
     all_files = []
-    files = dir.files.all
+    files = dir.files.all(prefix: prefix)
     truncated = files.is_truncated
 
     all_files += files.to_a
 
-    while truncated && all_files.size < 200000
-      puts "#{all_files.size} files"
+    while truncated && all_files.size < limit
+      pid, bytes_used = `ps ax -o pid,rss | grep -E "^[[:space:]]*#{Process::pid}"`.
+        chomp.
+        split(/\s+/).
+        map {|s| s.strip.to_i}
+      
+      puts "#{all_files.size} files (pid: #{pid} ram: #{(bytes_used / 1024.0).round(1)} Mb) s3://#{key}/#{prefix}"
       set = dir.files.all( :marker => files.last.key )
       all_files += set.to_a
       truncated = set.is_truncated
