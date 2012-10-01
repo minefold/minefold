81d7b1418d88994b65c2b1f4b1b58bd58418bdb5
Chris Lloyd <christopher.lloyd@gmail.com>
2012-10-01 15:40:36 -0700

    Mediocre support!
    Taken and refactored from @whatupdave's awesome commits https://github.com/minefold/web/compare/1a8f7ac67e28d682434d44e13c98f293fa67a993...adecf2830a8e64f9fa2731fcd26f9de5e119abe3

--- app/helpers/intercom_helper.rb
+++ app/helpers/intercom_helper.rb
@@ -1,13 +1,16 @@
 module IntercomHelper
-
+  
+  def intercom_activator(selector)
+    @intercom_activator = selector
+  end
+  
   def intercom_settings
-    settings = {
-      app_id: ENV['INTERCOM'],
-      widget: {
-        activator: '.ActivateIntercom'
-      }
-    }
-
+    settings = { app_id: ENV['INTERCOM'] }
+    
+    if @intercom_activator
+      settings[:widget] = { activator: @intercom_activator }
+    end
+    
     if signed_in?
       settings[:email] = current_user.email
       settings[:name] = current_user.name || current_user.username
@@ -15,22 +18,10 @@ module IntercomHelper
       settings[:user_hash] = Digest::SHA1.hexdigest('kczzht6c' + current_user.email)
 
       settings[:custom_data] = {
-        # 'beta?' => current_user.beta?,
-        # 'created worlds' => current_user.created_worlds.count,
+        'id' => current_user.id,
         'credits' => current_user.credits,
-        # 'facebook?' => current_user.facebook_linked?,
-        # 'minutes played' => current_user.minutes_played,
-        # 'max world players' => current_user.world_player_counts.max,
-        # 'member worlds' => current_user.worlds.count,
-        # 'pro?' => current_user.pro?,
         'username' => current_user.username,
-        # 'verify host' => current_user.verification_host,
-        # 'verified?' => current_user.verified?,
       }
-
-      # if current_user.verified?
-      #   settings[:custom_data][:profile] = player_url(current_user.minecraft_player)
-      # end
     end
 
     settings
