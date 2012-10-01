76f4b2731075a4a41a92e0592ea7179e75345007
Chris Lloyd <christopher.lloyd@gmail.com>
2012-10-01 01:08:48 -0400

    Unfuck homepage tests.

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -4,6 +4,8 @@ class PagesControllerTest < ActionController::TestCase
 
   %w(home about support pricing jobs privacy terms).each do |page|
     test "GET ##{page}" do
+      minecraft = Game.make!(:minecraft)
+      
       get page
       assert_response :success
     end
