ee733516283686bd93e54027589413dc247ee4ad
Chris Lloyd <christopher.lloyd@gmail.com>
2012-10-01 00:36:26 -0400

    Link to buy more credits on settings page.

--- app/helpers/credits_helper.rb
+++ app/helpers/credits_helper.rb
@@ -1,7 +1,13 @@
 # Handles anything to do with forrectly formatting
 
 module CreditsHelper
-
+  
+  def link_to_buy_credits
+    content_tag :a, 'Buy more credits',
+      href: new_orders_path,
+      data: { toggle: 'modal', target: '#buy-credits-modal' }
+  end
+  
   def format_credits(credits)
     number_with_delimiter(credits)
   end
