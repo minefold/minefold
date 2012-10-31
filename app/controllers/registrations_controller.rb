e1f4449de33d88913dfbb53b46fc5422e7b105da
Chris Lloyd <christopher.lloyd@gmail.com>
2012-10-30 20:30:13 -0700

    Widen footer to fulls screen.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
-
+  
+  def new
+    # TODO Move this hack into User.new_with_session(hash, session)
+    if mixpanel_cookie.present?
+      @distinct_id = JSON.parse(mixpanel_cookie)['distinct_id']
+    end
+    
+    super
+  end
+  
   def after_sign_up_path_for(resource)
     onboard_users_path
+  end
+
+# --
+
+  def mixpanel_cookie
+    request.cookies['mp_mixpanel']
   end
 
 end
