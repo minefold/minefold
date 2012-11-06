cc480245ba48b436f363a30e5dbf05ca20589a0c
Chris Lloyd <christopher.lloyd@gmail.com>
2012-11-05 21:38:32 -0800

    Form errors!

--- lib/bootstrap_form_helpers.rb
+++ lib/bootstrap_form_helpers.rb
@@ -5,9 +5,9 @@ module BootstrapFormHelpers
   def control_group(*fields, &blk)
     group_class = ['control-group']
 
-    # if object.errors.any? and fields.any? {|field| self.object.errors[field].any? }
-    #   group_class << 'error'
-    # end
+    if object.errors.any? and fields.any? {|field| self.object.errors[field].any? }
+      group_class << 'error'
+    end
 
     @template.content_tag(:div, class: group_class, &blk)
   end
