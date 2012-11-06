574fcf6dc4a02a01d4c59895e75fba515b40953a
Chris Lloyd <christopher.lloyd@gmail.com>
2012-11-05 20:38:03 -0800

    Skip out on the welcome page for the moment.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
-  
+
   def new
     # TODO Move this hack into User.new_with_session(hash, session)
     if mixpanel_cookie.present?
       @distinct_id = JSON.parse(mixpanel_cookie)['distinct_id']
     end
-    
+
     super
   end
-  
+
   def after_sign_up_path_for(resource)
-    onboard_users_path
+    user_root_path
   end
 
 # --
