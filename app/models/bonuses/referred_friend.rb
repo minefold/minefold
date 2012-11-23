1f77365def5be64d1b9de9d3037ce52eb7b4250d
Chris Lloyd <christopher.lloyd@gmail.com>
2012-11-23 15:57:16 -0800

    Add the credit limit back to friend referrals.

--- app/models/bonuses/referred_friend.rb
+++ app/models/bonuses/referred_friend.rb
@@ -1,8 +1,14 @@
 class Bonuses::ReferredFriend < Bonus
   self.credits = 500
 
+  LIMIT = 16
+
+  def self.credit_limit
+    credits * LIMIT
+  end
+
   def claimable?
-    user.bonuses.where(type: self.class).count < 16
+    self.class.where(user_id: user.id).count < LIMIT
   end
 
 end
