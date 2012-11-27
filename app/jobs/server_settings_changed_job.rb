f67d91c39b25b557132aee6fa2af810011caa058
Dave Newman <dave@snappyco.de>
2012-11-26 22:00:01 -0800

    prelim support for settings_changed

--- app/jobs/server_settings_changed_job.rb
+++ app/jobs/server_settings_changed_job.rb
@@ -1,12 +1,25 @@
 class ServerSettingsChangedJob < Job
   @queue = :high
 
-  def initialize(id, settings)
+  def initialize(id, change)
     @server = Server.find_by_party_cloud_id(id)
-    @settings = settings
+    @change = change
   end
 
   def perform!
+    # hack for whitelist_add, whitelist_remove, blacklist_add, blacklist_remove, ops_add, ops_remove
+    if @change['setting'] =~ /([a-z]+)_add/
+      set = @server.settings[$1].split("\n")
+      @server.settings[$1] = (set | [@change['value']]).uniq.join("\n")
+
+    elsif @change['setting'] =~ /([a-z]+)_remove/
+      set = @server.settings[$1].split("\n")
+      @server.settings[$1] = (set - [@change['value']]).uniq.join("\n")
+
+    else
+      @server.settings[@change['setting']] = @change['value']
+    end
+    @server.save!
   end
 
 end
