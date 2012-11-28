e6a767ceb660684d0893739311a694c6bfde2fb5
Chris Lloyd <christopher.lloyd@gmail.com>
2012-11-27 18:44:00 -0800

    Regen Minefold session secret.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,4 +2,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Minefold::Application.config.secret_token = '05ae72939ba3a412f14af074569d9b8f6431d164dec4017405f532ed0fa85a254a0914262be1561cf4cf9acf593509e90c2838d6cb3ed44c1039bdbf0c336510'
+Minefold::Application.config.secret_token = 'e2083741107fefbe62eaaf7677ec1487b40f108259b782ea69940d1beb51300544d503da73b1c496180656b04dea6933a2cd18e7c86c68f2dfaca023aad09e68'
