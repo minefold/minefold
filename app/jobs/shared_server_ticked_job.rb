2a96205e6515a387edbae96b70f297e840f0bbad
Dave Newman <dave@snappyco.de>
2012-11-28 18:18:19 -0800

    fix shared server tick job

--- app/jobs/shared_server_ticked_job.rb
+++ app/jobs/shared_server_ticked_job.rb
@@ -1,10 +1,10 @@
 class SharedServerTickedJob < Job
   @queue = :high
 
-  def initialize(server_pc_id, player_uids, timestamp)
+  def initialize(server_pc_id, player_ids, timestamp)
     @server = Server.unscoped.find_by_party_cloud_id(server_pc_id)
     # TODO look up game type
-    @players = @server.funpack.game.players.where(uid: player_uids).all
+    @players = @server.funpack.game.players.where(id: player_ids).all
     @timestamp = Time.at(timestamp)
   end
 
