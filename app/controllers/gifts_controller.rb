781f486227a74e5bee34f130bc2806ee0d716d0d
Chris Lloyd <christopher.lloyd@gmail.com>
2012-12-07 16:08:22 -0800

    Fixes.

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -13,14 +13,19 @@ class GiftsController < ApplicationController
       gift.customer_id = current_user.customer_id
     end
 
-    gift.valid? and gift.fulfill && gift.save
+    if gift.valid? and gift.fulfill && gift.save
 
-    GiftsMailer.receipt(gift.id).deliver
+      GiftsMailer.receipt(gift.id).deliver
 
-    track 'Purchased', 'gift' => gift.id,
-                       'existing customer' => gift.parent_id
+      track 'Purchased', 'gift' => gift.id,
+                         'existing customer' => gift.parent_id
+      respond_with(gift, location: xmas_cheers_path(gift))
+    else
+      flash[:error] = "There was an error charing your card. Please try another card or contact support."
 
-    respond_with(gift, location: xmas_cheers_path(gift))
+      redirect_to xmas_promo_path
+    end
+
   end
 
   def cheers
