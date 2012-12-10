60ce3703c3b4a2aeb67eb44689ac047438e1b100
Chris Lloyd <christopher.lloyd@gmail.com>
2012-12-09 18:33:49 -0800

    Vote on connection.

--- app/jobs/player_connected_job.rb
+++ app/jobs/player_connected_job.rb
@@ -1,11 +1,18 @@
 class PlayerConnectedJob < Job
   @queue = :high
 
-  def initialize(server_pc_id, uid)
+  def initialize(server_pc_id, uid, timestamp)
     @server = Server.unscoped.find_by_party_cloud_id(server_pc_id)
     @player = Player.minecraft.find_by_uid(uid)
+    @timestamp = timestamp
   end
 
   def perform!
+
+    vote = Vote.new
+    vote.server = @server
+    vote.user = @player.user
+    vote.save!
+
   end
 end
