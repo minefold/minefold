f990e6665036f2805f7fd65f9e8013483181272b
Chris Lloyd <christopher.lloyd@gmail.com>
2012-12-18 07:08:24 -0800

    Fix.

--- app/jobs/player_connected_job.rb
+++ app/jobs/player_connected_job.rb
@@ -7,7 +7,7 @@ class PlayerConnectedJob < Job
 
   def initialize(timestamp, party_cloud_id, uid)
     @time = timestamp
-    @server = Server.unscoped.find_by_party_cloud_id(server_pc_id)
+    @server = Server.unscoped.find_by_party_cloud_id(party_cloud_id)
     # TODO Find from game auth type
     @account = Accounts::Mojang.find_or_create_by_uid(uid)
   end
