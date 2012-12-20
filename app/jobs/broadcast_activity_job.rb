cef11989b70d0b225e72166cd80ada366f303772
Chris Lloyd <christopher.lloyd@gmail.com>
2012-12-20 09:05:07 -0800

    STUPID.

---
+++ app/jobs/broadcast_activity_job.rb
@@ -0,0 +1,13 @@
+class BroadcastActivityJob < Job
+
+  attr_reader :activity
+
+  def initialize(klass, id)
+    @activity = klass.constantize.find(id)
+  end
+
+  def perform!
+    activity.broadcast
+  end
+
+end
