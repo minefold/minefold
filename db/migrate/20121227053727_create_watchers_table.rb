24197caadec4ef8b7069e4bf0488cbfebca220f3
Chris Lloyd <christopher.lloyd@gmail.com>
2012-12-27 14:53:10 -0800

    Don't create watchers in migration.

--- db/migrate/20121227053727_create_watchers_table.rb
+++ db/migrate/20121227053727_create_watchers_table.rb
@@ -5,21 +5,6 @@ class CreateWatchersTable < ActiveRecord::Migration
       t.references :user
       t.references :server
     end
-
-    Server.find_each do |server|
-      whitelist = (server.settings['whitelist'] || "").split(/\r\n|\n/)
-      ops = (server.settings['ops'] || "").split(/\r\n|\n/)
-
-      users = [server.creator] + (whitelist | ops).map {|username|
-        account = Accounts::Mojang.where(uid: username).first
-        account && account.user
-      }.compact.uniq
-
-      users.each do |user|
-        server.watchers << user
-      end
-    end
-
   end
 
 end
