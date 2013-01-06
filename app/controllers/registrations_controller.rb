0d12f3f7c11ae848ba17a061c966fb7e2619bff3
Dave Newman <dave@snappyco.de>
2013-01-06 14:00:35 -0800

    add sign up conversion param for analytics

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
 # --
 
   def after_sign_up_path_for(resource)
-    user_root_path
+    user_root_path(event: 'welcome') # welcome param triggers conversion tracking
   end
 
 # --
