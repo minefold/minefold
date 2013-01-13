0635d447c982794b3263ce0098d1919c1f1caf26
Chris Lloyd <christopher.lloyd@gmail.com>
2013-01-13 13:16:13 -0800

    Fix server spec.

--- spec/models/server_spec.rb
+++ spec/models/server_spec.rb
@@ -11,25 +11,4 @@ describe Server do
     end
   end
 
-  describe "#auth_type" do
-    let(:server) { Server.new }
-
-    it "returns the auth class" do
-      expect(server.auth_type).to eq(ServerAuths::MinecraftPrivate)
-
-      server.auth_type_enum = 1
-      expect(server.auth_type).to eq(ServerAuths::MinecraftPublic)
-    end
-  end
-
-  describe "#auth_type=" do
-    let(:server) { Server.new }
-
-    it "clears the auth cache" do
-      expect {
-        server.auth_type = ServerAuths::MinecraftPublic
-      }.to change(server, :auth)
-    end
-  end
-
 end
