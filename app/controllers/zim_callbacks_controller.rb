3096556ec106782570ec7e441e71d0f547812440
Dave Newman <dave@snappyco.de>
2013-01-22 17:55:56 -0800

    turn off csrf for webhook

--- app/controllers/zim_callbacks_controller.rb
+++ app/controllers/zim_callbacks_controller.rb
@@ -1,11 +1,13 @@
 class ZimCallbacksController < ApplicationController
   respond_to :json
+  
+  skip_before_filter  :verify_authenticity_token
 
   def map_deleted
     data = JSON.parse(request.body.read)
 
     if @server = Server.unscoped.find(data['id'])
-      @server.world.destroy
+      @server.world.destroy if @server.world
     end
 
     render nothing: true, :status => :ok
