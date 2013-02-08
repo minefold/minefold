5fb693826aa210bd448a4325a767ce583817044c
Dave Newman <dave@snappyco.de>
2013-02-08 14:44:02 -0800

    :tf2 => true

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -3,6 +3,10 @@ adapter = Flipper::Adapters::Redis.new(redis)
 
 $flipper = Flipper.new(adapter)
 
+Flipper.register(:all) do |actor|
+  true
+end
+
 Flipper.register(:admins) do |actor|
   actor.respond_to?(:admin?) && actor.admin?
 end
@@ -14,4 +18,4 @@ end
 # --
 
 $flipper[:tf2].enable $flipper.group(:admins)
-$flipper[:tf2].enable $flipper.group(:beta)
+$flipper[:tf2].enable $flipper.group(:all)
