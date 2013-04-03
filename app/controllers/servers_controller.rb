db52a4ee8c0912a7ca4e7a358a777db98eb14e31
Chris Lloyd <christopher.lloyd@gmail.com>
2013-04-03 15:44:07 -0700

    D'oh, actually save server with party cloud id.

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -36,6 +36,7 @@ class ServersController < ApplicationController
 
     if server.save
       server.party_cloud_id ||= PartyCloud::Server.create(server.funpack, server.name).id
+      server.save
 
       track server.creator.distinct_id, 'Created server',
         name: server.name,
