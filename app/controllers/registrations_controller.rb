7b3047339598413066e882c89056a3a5fdb8fa44
Dave Newman <dave@snappyco.de>
2013-05-06 14:13:43 -0700

    a/b test button position on home page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,7 @@ class RegistrationsController < Devise::RegistrationsController
 # --
 
   def after_sign_up_path_for(resource)
+    flash[:signed_up] = true
     user_root_path(event: 'welcome') # welcome param triggers conversion tracking
   end
 
