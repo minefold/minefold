c8f16400f3a84b33e1c1fe5fb95a34c364dcfcb4
Dave Newman <dave@snappyco.de>
2013-08-27 13:19:16 -0700

    add rake task for deleting snaps

--- app/jobs/delete_old_snapshots_job.rb
+++ app/jobs/delete_old_snapshots_job.rb
@@ -1,11 +1,15 @@
 # should be run about once a day
-
 class DeleteOldSnapshotsJob < Job
   @queue = :low
 
   def perform
-    Server.unscoped.where('deleted_at > ? and deleted_at < ?', 16.days.ago, 14.days.ago).each do |s|
-      PartyCloud::Server.new(s.party_cloud_id).delete_all_snapshots!
+    pc_ids = Server.unscoped.where(
+      'party_cloud_id is not null and deleted_at > ? and deleted_at < ?',
+      16.days.ago, 14.days.ago
+    ).select(:party_cloud_id).map(&:party_cloud_id)
+
+    while pc_id = pc_ids.pop
+      PartyCloud::Server.new(pc_id).delete_all_snapshots!
     end
   end
 end
