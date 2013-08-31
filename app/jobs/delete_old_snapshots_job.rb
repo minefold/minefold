f979ec2dc844e481d3cff41b8002465a033787a3
Dave Newman <dave@snappyco.de>
2013-08-31 16:51:52 -0700

    delete free servers that haven't been played in 6 months

--- app/jobs/delete_old_snapshots_job.rb
+++ app/jobs/delete_old_snapshots_job.rb
@@ -3,6 +3,11 @@ class DeleteOldSnapshotsJob < Job
   @queue = :low
 
   def perform
+    delete_free_servers_older_than(6.months.ago)
+    delete_snapshots_for_deleted_servers
+  end
+
+  def delete_snapshots_for_deleted_servers
     pc_ids = Server.unscoped.where(
       'party_cloud_id is not null and deleted_at > ? and deleted_at < ?',
       16.days.ago, 14.days.ago
@@ -10,6 +15,20 @@ class DeleteOldSnapshotsJob < Job
 
     while pc_id = pc_ids.pop
       PartyCloud::Server.new(pc_id).delete_all_snapshots!
+    end
+  end
+
+  def delete_free_servers_older_than(time)
+    Server.
+      joins('left outer join worlds on worlds.server_id = servers.id').
+      joins('inner join users on servers.creator_id = users.id').
+      joins('left outer join subscriptions on users.subscription_id = subscriptions.id').
+      where('servers.party_cloud_id is not null').
+      where('servers.updated_at < ? and (worlds.id is null or worlds.updated_at < ?)', time, time).
+      where('servers.minutes_played < ?', 10 * 60).
+      where('subscriptions.id is null or users.total_trial_time = 0').find_each do |s|
+        # puts "#{s.id}   #{s.minutes_played}  #{s.updated_at}"
+        s.delete
     end
   end
 end
